1ef40c0c262cbbc2d56d29571276484428682c2f
gsierra22 <giancarlosierra2011@hotmail.com>
2022-11-09 23:05:06 -0600

    pet router cleanup

--- server/routes/pets.router.js
+++ server/routes/pets.router.js
@@ -4,8 +4,6 @@ const router = express.Router();
 
 //get route for personal pets
 router.get("/mypets/:id", (req, res) => {
-  //console.log('hello')
-  //console.log(req.query)
   const queryText = ` SELECT * FROM "user"
   JOIN "pets" ON "user".id=pets.user_id
   WHERE "user".id=$1`;
@@ -22,7 +20,6 @@ router.get("/mypets/:id", (req, res) => {
 
 //get route for all pets
 router.get("/all", (req, res) => {
-  //console.log('arrive in router.get')
   const query = `SELECT * FROM pets`;
   pool
     .query(query)
@@ -38,7 +35,6 @@ router.get("/all", (req, res) => {
 
 //post route for new pets
 router.post("/", (req, res) => {
-  //console.log(req.body);
   // RETURNING "id" will give us back the id of the created pet
   const insertPetQuery = `INSERT INTO "pets" (name, catdog, missing, description, neighborhood, photo, missing_message, user_id) 
   VALUES ( $1, $2, $3, $4, $5, $6, $7, $8 ) RETURNING "id";
@@ -57,7 +53,6 @@ router.post("/", (req, res) => {
       req.body.user_id,
     ])
     .then((result) => {
-      //console.log("New Pet Id:", result.rows[0].id)
       const createdPetId = result.rows[0].id;
 
       // Now handle the track reference
